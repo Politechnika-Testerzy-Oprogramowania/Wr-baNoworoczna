ba58fe75f6244f224e8855dfad9a5cb457664626
LeszSur <95530259+LeszSur@users.noreply.github.com>
2021-12-05 14:49:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,41 +4,45 @@
     let war_3_status = document.getElementById('status')
     let zodiak = document.getElementById('zodiak')
     let wiek = document.getElementById('wiek')
-    let wybor;
 
    /* 
    function ktora_wrozba(){
-   let ....
-    if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "student") wybor = "kw1";
-    else if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "pracuje") wybor = "kw2";
-    else if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "szuka_pracy") wybor = "kw3";
-    
-    else if (war_1_plec == "kobieta" && war_2_stan == "zajety" && war_3_status == "student") wybor = "kr1";
-    else if (war_1_plec == "kobieta" && war_2_stan == "zajety" && war_3_status == "pracuje") wybor = "kr2";
-    else if (war_1_plec == "kobieta" && war_2_stan == "zajety" && war_3_status == "szuka_pracy") wybor = "kr3";
+        let wybor
+        let par1w = war_1_plec.value;
+        let par2w = war_2_stan.value;
+        let par3w = war_3_status.value;
+        
+            if (par1w == "kobieta" && par2w == "wolny" && par3w == "student") wybor = "kw1";
+            else if (par1w == "kobieta" && par2w == "wolny" && par3w == "pracuje") wybor = "kw2";
+            else if (par1w == "kobieta" && par2w == "wolny" && par3w == "szuka_pracy") wybor = "kw3";
 
-    else if (war_1_plec == "kobieta" && war_2_stan == "zwiazek" && war_3_status == "student") wybor = "kn1";
-    else if (war_1_plec == "kobieta" && war_2_stan == "zwiazek" && war_3_status == "pracuje") wybor = "kn2";
-    else if (war_1_plec == "kobieta" && war_2_stan == "zwiazek" && war_3_status == "szuka_pracy") wybor = "kn3";
+            else if (par1w == "kobieta" && par2w == "zajety" && par3w == "student") wybor = "kr1";
+            else if (par1w == "kobieta" && par2w == "zajety" && par3w == "pracuje") wybor = "kr2";
+            else if (par1w == "kobieta" && par2w == "zajety" && par3w == "szuka_pracy") wybor = "kr3";
+
+            else if (par1w == "kobieta" && par2w == "zwiazek" && par3w == "student") wybor = "kn1";
+            else if (par1w == "kobieta" && par2w == "zwiazek" && par3w == "pracuje") wybor = "kn2";
+            else if (par1w == "kobieta" && par2w == "zwiazek" && par3w == "szuka_pracy") wybor = "kn3";
 
 
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "wolny" && war_3_status == "student") wybor = "mw1";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "wolny" && war_3_status == "pracuje") wybor = "mw2";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "wolna_wolny" && war_3_status == "szuka_pracy") wybor = "mw3";
-    
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "zajety" && war_3_status == "student") wybor = "mr1";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "zajety" && war_3_status == "pracuje") wybor = "mr2";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "zajety" && war_3_status == "szuka_pracy") wybor = "mr3";
+            else if (par1w == "mezczyzna" && par2w == "wolny" && par3w == "student") wybor = "mw1";
+            else if (par1w == "mezczyzna" && par2w == "wolny" && par3w == "pracuje") wybor = "mw2";
+            else if (par1w == "mezczyzna" && par2w == "wolny" && par3w == "szuka_pracy") wybor = "mw3";
 
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "student") wybor = "mn1";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "pracuje") wybor = "mn2";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "szuka_pracy") wybor = "mn3"; 
+            else if (par1w == "mezczyzna" && par2w == "zajety" && par3w == "student") wybor = "mr1";
+            else if (par1w == "mezczyzna" && par2w == "zajety" && par3w == "pracuje") wybor = "mr2";
+            else if (par1w == "mezczyzna" && par2w == "zajety" && par3w == "szuka_pracy") wybor = "mr3";
+
+            else if (par1w == "mezczyzna" && par2w == "zwiazek" && par3w == "student") wybor = "mn1";
+            else if (par1w == "mezczyzna" && par2w == "zwiazek" && par3w == "pracuje") wybor = "mn2";
+            else if (par1w == "mezczyzna" && par2w == "zwiazek" && par3w == "szuka_pracy") wybor = "mn3"; 
     
     return ....
     }
     */ 
 
-    function opracuj_wrozbe(wynik){
+    function opracuj_wrozbe(){
+        let wynik;
         let par1 = war_1_plec.value;
         let par2 = war_2_stan.value;
         let par3 = war_3_status.value;
