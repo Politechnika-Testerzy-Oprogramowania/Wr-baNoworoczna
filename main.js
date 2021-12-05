4f338f4ddddb060f799f7fe2b01dcb64663ee879
LeszSur <95530259+LeszSur@users.noreply.github.com>
2021-12-05 14:36:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,10 @@
     let wiek = document.getElementById('wiek')
     let wybor;
 
-   /* if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "student") wybor = "kw1";
+   /* 
+   function ktora_wrozba(){
+   let ....
+    if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "student") wybor = "kw1";
     else if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "pracuje") wybor = "kw2";
     else if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "szuka_pracy") wybor = "kw3";
     
@@ -29,17 +32,21 @@
 
     else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "student") wybor = "mn1";
     else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "pracuje") wybor = "mn2";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "szuka_pracy") wybor = "mn3"; */ 
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "szuka_pracy") wybor = "mn3"; 
+    
+    return ....
+    }
+    */ 
 
     function opracuj_wrozbe(wynik){
         let par1 = war_1_plec.value;
         let par2 = war_2_stan.value;
         let par3 = war_3_status.value;
         if (par1 == "" || par2 == "" || par3 == "") wynik = "Nie podałeś istotnych parametrów";
+         //else .... wynik = ktora_wrozba();
         return wynik;
     }
 
     function wyswietl_wrozbe(){
-        //document.getElementById("wrozba").innerHTML = opracuj_wrozbe();
-        console.log(opracuj_wrozbe());
+        document.getElementById("wrozba").innerHTML = opracuj_wrozbe();
     }
