a820fea3695c06683c7d11037ea4acf00400e52e
LeszSur <95530259+LeszSur@users.noreply.github.com>
2021-12-05 14:25:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,28 +6,40 @@
     let wiek = document.getElementById('wiek')
     let wybor;
 
-    if (war_1_plec == "kobieta" && war_2_stan == "wolna_wolny" && war_3_status == "student") wybor = "kw1";
-    else if (war_1_plec == "kobieta" && war_2_stan == "wolna_wolny" && war_3_status == "pracuje") wybor = "kw2";
-    else if (war_1_plec == "kobieta" && war_2_stan == "wolna_wolny" && war_3_status == "szuka_pracy") wybor = "kw3";
+   /* if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "student") wybor = "kw1";
+    else if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "pracuje") wybor = "kw2";
+    else if (war_1_plec == "kobieta" && war_2_stan == "wolny" && war_3_status == "szuka_pracy") wybor = "kw3";
     
-    else if (war_1_plec == "kobieta" && war_2_stan == "mezatka_zonaty" && war_3_status == "student") wybor = "kr1";
-    else if (war_1_plec == "kobieta" && war_2_stan == "mezatka_zonaty" && war_3_status == "pracuje") wybor = "kr2";
-    else if (war_1_plec == "kobieta" && war_2_stan == "mezatka_zonaty" && war_3_status == "szuka_pracy") wybor = "kr3";
+    else if (war_1_plec == "kobieta" && war_2_stan == "zajety" && war_3_status == "student") wybor = "kr1";
+    else if (war_1_plec == "kobieta" && war_2_stan == "zajety" && war_3_status == "pracuje") wybor = "kr2";
+    else if (war_1_plec == "kobieta" && war_2_stan == "zajety" && war_3_status == "szuka_pracy") wybor = "kr3";
 
-    else if (war_1_plec == "kobieta" && war_2_stan == "nieformalny" && war_3_status == "student") wybor = "kn1";
-    else if (war_1_plec == "kobieta" && war_2_stan == "nieformalny" && war_3_status == "pracuje") wybor = "kn2";
-    else if (war_1_plec == "kobieta" && war_2_stan == "nieformalny" && war_3_status == "szuka_pracy") wybor = "kn3";
+    else if (war_1_plec == "kobieta" && war_2_stan == "zwiazek" && war_3_status == "student") wybor = "kn1";
+    else if (war_1_plec == "kobieta" && war_2_stan == "zwiazek" && war_3_status == "pracuje") wybor = "kn2";
+    else if (war_1_plec == "kobieta" && war_2_stan == "zwiazek" && war_3_status == "szuka_pracy") wybor = "kn3";
 
 
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "wolna_wolny" && war_3_status == "student") wybor = "mw1";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "wolna_wolny" && war_3_status == "pracuje") wybor = "mw2";
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "wolny" && war_3_status == "student") wybor = "mw1";
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "wolny" && war_3_status == "pracuje") wybor = "mw2";
     else if (war_1_plec == "mezczyzna" && war_2_stan == "wolna_wolny" && war_3_status == "szuka_pracy") wybor = "mw3";
     
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "mezatka_zonaty" && war_3_status == "student") wybor = "mr1";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "mezatka_zonaty" && war_3_status == "pracuje") wybor = "mr2";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "mezatka_zonaty" && war_3_status == "szuka_pracy") wybor = "mr3";
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "zajety" && war_3_status == "student") wybor = "mr1";
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "zajety" && war_3_status == "pracuje") wybor = "mr2";
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "zajety" && war_3_status == "szuka_pracy") wybor = "mr3";
 
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "nieformalny" && war_3_status == "student") wybor = "mn1";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "nieformalny" && war_3_status == "pracuje") wybor = "m22";
-    else if (war_1_plec == "mezczyzna" && war_2_stan == "nieformalny" && war_3_status == "szuka_pracy") wybor = "m33";  
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "student") wybor = "mn1";
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "pracuje") wybor = "mn2";
+    else if (war_1_plec == "mezczyzna" && war_2_stan == "zwiazek" && war_3_status == "szuka_pracy") wybor = "mn3"; */ 
 
+    function opracuj_wrozbe(wynik){
+        let par1 = war_1_plec.value;
+        let par2 = war_2_stan.value;
+        let par3 = war_3_status.value;
+        if (par1 == "" || par2 == "" || par3 == "") wynik = "Nie podałeś istotnych parametrów";
+        return wynik;
+    }
+
+    function wyswietl_wrozbe(){
+        //document.getElementById("wrozba").innerHTML = opracuj_wrozbe();
+        console.log(opracuj_wrozbe());
+    }
