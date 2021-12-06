da6a0d663cb7ea8583b22a2e7ec2b7d1aeb22c34
MartaP21 <95525483+MartaP21@users.noreply.github.com>
2021-12-06 18:03:18 +0100

    Update main.js
    Poprawione na 3 krótkie wróżby.

--- main.js
+++ main.js
@@ -1,69 +1,50 @@
 // Wróżba Noworoczna Main.js
 
-   function ktora_wrozba(){
-
-        let par1w = document.getElementById('plec').value;
-        let par2w = document.getElementById('stan').value;
-        let par3w = document.getElementById('status').value;
-        let wybor, wybor2, wybor3;
-        
-            if (par1w == "kobieta") wybor = "kw1";
-            else  (par1w == "mezczyzna") wybor = "kw2";
-           
-            if (par2w == "zajety") wybor2 = "kr1";
-            else if (par2w == "wolny") wybor2 = "kr2";
-            else if (par2w == "zwiazek") wybor2 = "kr3";
-
-            if (par3w == "student") wybor3 = "kn1";
-            else if (par3w == "pracuje") wybor3 = "kn2";
-            else if (par3w == "szuka_pracy") wybor3 = "kn3";
-      
-            /* if (par1w == "kobieta" && par2w == "wolny" && par3w == "student") wybor = "kw1";
-            else if (par1w == "kobieta" && par2w == "wolny" && par3w == "pracuje") wybor = "kw2";
-            else if (par1w == "kobieta" && par2w == "wolny" && par3w == "szuka_pracy") wybor = "kw3";
-
-            else if (par1w == "kobieta" && par2w == "zajety" && par3w == "student") wybor = "kr1";
-            else if (par1w == "kobieta" && par2w == "zajety" && par3w == "pracuje") wybor = "kr2";
-            else if (par1w == "kobieta" && par2w == "zajety" && par3w == "szuka_pracy") wybor = "kr3";
-
-            else if (par1w == "kobieta" && par2w == "zwiazek" && par3w == "student") wybor = "kn1";
-            else if (par1w == "kobieta" && par2w == "zwiazek" && par3w == "pracuje") wybor = "kn2";
-            else if (par1w == "kobieta" && par2w == "zwiazek" && par3w == "szuka_pracy") wybor = "kn3";
-
-
-            else if (par1w == "mezczyzna" && par2w == "wolny" && par3w == "student") wybor = "mw1";
-            else if (par1w == "mezczyzna" && par2w == "wolny" && par3w == "pracuje") wybor = "mw2";
-            else if (par1w == "mezczyzna" && par2w == "wolny" && par3w == "szuka_pracy") wybor = "mw3";
-
-            else if (par1w == "mezczyzna" && par2w == "zajety" && par3w == "student") wybor = "mr1";
-            else if (par1w == "mezczyzna" && par2w == "zajety" && par3w == "pracuje") wybor = "mr2";
-            else if (par1w == "mezczyzna" && par2w == "zajety" && par3w == "szuka_pracy") wybor = "mr3";
-
-            else if (par1w == "mezczyzna" && par2w == "zwiazek" && par3w == "student") wybor = "mn1";
-            else if (par1w == "mezczyzna" && par2w == "zwiazek" && par3w == "pracuje") wybor = "mn2";
-            else if (par1w == "mezczyzna" && par2w == "zwiazek" && par3w == "szuka_pracy") wybor = "mn3"; */ 
+function plec(){
+    let par1w = document.getElementById('plec').value;
+    let wybor;
     
-    return wybor, wybor2, wybor3;
-    }
-    
-    function twojWiek(){
-        let data_dzis= new Date();
-        let data_uzytkownika= document.getElementById('urodziny').value;
-        let wiek = data_dzis - data_uzytkownika;
-        wiek = Math.floor(wiek/(1000*360*60*60*24));
-        return wiek;
-    }
-    function opracuj_wrozbe(){
-        let war_1_plec = document.getElementById('plec').value;
-        let war_2_stan = document.getElementById('stan').value;
-        let war_3_status = document.getElementById('status').value;
-        let zodiak = document.getElementById('zodiak').value;
-        let wynik;
-        if (war_1_plec == "" || war_2_stan == "" || war_3_status == "" || zodiak == "")  {wynik = "Nie podałeś wszystkich parametrów, spróbuj jeszcze raz"}
-        else {wynik = "Drogi/a  " + zodiak + " masz już " + twojWiek() + " lat, a rok 2022 szykuje dla Ciebie masę niespodzianek. " + ktora_wrozba()};
-        return wynik;
-    }
+        if (par1w == "kobieta") wybor = "Super, jesteś kobietą.";
+        else if (par1w == "mezczyzna") wybor = "Fajnie, jesteś facetem.";
+    return wybor;
+}
+function stan(){
+    let par2w = document.getElementById('stan').value;
+    let wybor2;
+        if (par2w == "zajety") wybor2 = "Brawo, przebywasz w formalnym związku.";
+        else if (par2w == "wolny") wybor2 = "Cieszysz się życiem jako singiel.";
+        else if (par2w == "zwiazek") wybor2 = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia.";
+    return wybor2;
+}
+function status2(){
+    let par3w = document.getElementById('status').value;
+    let wybor3;
+        if (par3w == "student") wybor3 = "Studenckie życie to jest to.";
+        else if (par3w == "pracuje") wybor3 = "Pracujesz - prowadzisz stabilne życie.";
+        else if (par3w == "szuka_pracy") wybor3 = "Spełniasz marzenia szukając nowych szans.";
+    return wybor3;
+}
 
-    function wyswietl_wrozbe(){
-        document.getElementById("wrozba").innerHTML = opracuj_wrozbe();
-    }
+function twojWiek(){
+    let data_dzis = new Date();
+    let d1 = data_dzis.getYear() + 1900;
+    let data_uzytkownika = new Date(document.getElementById('urodziny').value);
+    let d2 = data_uzytkownika.getYear() + 1900;
+    let wiek = d1 - d2;
+console.log(wiek);
+return wiek;
+}
+function opracuj_wrozbe(){
+    let war_1_plec = document.getElementById('plec').value;
+    let war_2_stan = document.getElementById('stan').value;
+    let war_3_status = document.getElementById('status').value;
+    let zodiak = document.getElementById('zodiak').value;
+    let wynik;
+    if (war_1_plec == "" || war_2_stan == "" || war_3_status == "" || zodiak == "")  {wynik = "Nie podałeś wszystkich parametrów, spróbuj jeszcze raz"}
+    else {wynik = "Drogi/a  " + zodiak + " masz już " + twojWiek() + " lat, a rok 2022 szykuje dla Ciebie masę niespodzianek. "+ "</br></br>" + plec()+ "</br></br>" + stan() + "</br></br>" + status2 ()};
+    return wynik;
+}
+
+function wyswietl_wrozbe(){
+    document.getElementById("wrozba").innerHTML = opracuj_wrozbe();
+}
