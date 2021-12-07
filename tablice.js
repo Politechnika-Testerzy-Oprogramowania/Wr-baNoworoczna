722d60d58b707145bee350e3f188f2e3dd6a63d6
LeszSur <95530259+LeszSur@users.noreply.github.com>
2021-12-07 13:39:55 +0100

    Add files via upload

---
+++ tablice.js
@@ -0,0 +1,5 @@
+
+  let baza_kobieta = new Array(3);
+        baza_kobieta[0] = "Super, jesteś fajną kobietą";
+	baza_kobieta[1] = "Super, jesteś wystrzałową kobietą";
+	baza_kobieta[2] = "Super, jesteś wspaniałą kobietą";
