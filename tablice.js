cb65dbd99a599b62ca7d648370dde9c8b08116be
Leszek Surazynski <lessurol@gmail.com>
2022-01-31 13:05:19 +0100

    poprawka tablic w tablice.js

--- tablice.js
+++ tablice.js
@@ -1,40 +1,40 @@
 
-  let baza_kobieta = new Array(3);
+  let baza_kobieta = [];
         baza_kobieta[0] = "Super, jesteś fajną kobietą";
 	      baza_kobieta[1] = "Super, jesteś wystrzałową kobietą";
 	      baza_kobieta[2] = "Super, jesteś wspaniałą kobietą";
 
-  let baza_mezczyzna = new Array(3);
+  let baza_mezczyzna = [];
         baza_mezczyzna[0] = "Fajnie że jesteś mężczyzną w1";
 	      baza_mezczyzna[1] = "Fajnie że jesteś mężczyzną w2";
 	      baza_mezczyzna[2] = "Fajnie że jesteś mężczyzną w3";
 
-  let baza_zajety = new Array(3);
+  let baza_zajety = [];
         baza_zajety[0] = "Brawo, przebywasz w formalnym związku w1";
 	      baza_zajety[1] = "Brawo, przebywasz w formalnym związku w2";
 	      baza_zajety[2] = "Brawo, przebywasz w formalnym związku w3";
 
-  let baza_wolny = new Array(3);
+  let baza_wolny = [];
         baza_wolny[0] = "Cieszysz się życiem jako singiel w1";
 	      baza_wolny[1] = "Cieszysz się życiem jako singiel w2";
 	      baza_wolny[2] = "Cieszysz się życiem jako singiel w3";  
 
-  let baza_zwiazek = new Array(3);
+  let baza_zwiazek = [];
         baza_zwiazek[0] = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia w1";
 	      baza_zwiazek[1] = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia w2";
 	      baza_zwiazek[2] = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia w3";
 
-  let baza_student = new Array(3);
+  let baza_student = [];
         baza_student[0] = "Studenckie życie to jest to w1";
 	      baza_student[1] = "Studenckie życie to jest to w2";
 	      baza_student[2] = "Studenckie życie to jest to w3";
         
-  let baza_pracuje = new Array(3);
+  let baza_pracuje = [];
         baza_pracuje[0] = "Pracujesz - prowadzisz stabilne życie w1";
 	      baza_pracuje[1] = "Pracujesz - prowadzisz stabilne życie w2";
 	      baza_pracuje[2] = "Pracujesz - prowadzisz stabilne życie w3";
 
-  let baza_szuka_pracy = new Array(3);
+  let baza_szuka_pracy = [];
         baza_szuka_pracy[0] = "Spełniasz marzenia szukając nowych szans w1";
 	      baza_szuka_pracy[1] = "Spełniasz marzenia szukając nowych szans w2";
 	      baza_szuka_pracy[2] = "Spełniasz marzenia szukając nowych szans w3";    
