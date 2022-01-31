f4b1444113f0d42833b0b47f093be151f1400be7
Leszek Surazynski <lessurol@gmail.com>
2022-01-31 13:40:27 +0100

    dopisane teksty wróżb

--- tablice.js
+++ tablice.js
@@ -1,40 +1,42 @@
 
   let baza_kobieta = [];
-        baza_kobieta[0] = "Super, jesteś fajną kobietą";
-	      baza_kobieta[1] = "Super, jesteś wystrzałową kobietą";
-	      baza_kobieta[2] = "Super, jesteś wspaniałą kobietą";
+        baza_kobieta[0] = "Super, jesteś wspaniałą kobietą";
+	      baza_kobieta[1] = "Ten rok z pewnością bedzie należał do Ciebie";
+	      baza_kobieta[2] = "Tym razem to Ty będziesz rozdawać karty";
+            baza_kobieta[3] = "W tym roku czeka na Ciebie wiele nowości";
 
   let baza_mezczyzna = [];
-        baza_mezczyzna[0] = "Fajnie że jesteś mężczyzną w1";
-	      baza_mezczyzna[1] = "Fajnie że jesteś mężczyzną w2";
-	      baza_mezczyzna[2] = "Fajnie że jesteś mężczyzną w3";
+        baza_mezczyzna[0] = "W tym roku czeka Cię wiele poważnych wyzwań";
+	      baza_mezczyzna[1] = "Czy chcesz, czy nie chcesz, ale ten rok przyniesie wiele nowości";
+	      baza_mezczyzna[2] = "To może być Ciekawy rok";
+            baza_mezczyzna[3] = "To bedzie Twój rok, ale musisz się postarać";
 
   let baza_zajety = [];
-        baza_zajety[0] = "Brawo, przebywasz w formalnym związku w1";
-	      baza_zajety[1] = "Brawo, przebywasz w formalnym związku w2";
-	      baza_zajety[2] = "Brawo, przebywasz w formalnym związku w3";
+        baza_zajety[0] = "Formalny związek da Ci wiele zadowolenia";
+	      baza_zajety[1] = "W Twoim związku wydarzy się coś nowego";
+	      baza_zajety[2] = "Rozejrzyj się wokół siebie, może czas na zmiany";
 
   let baza_wolny = [];
-        baza_wolny[0] = "Cieszysz się życiem jako singiel w1";
-	      baza_wolny[1] = "Cieszysz się życiem jako singiel w2";
-	      baza_wolny[2] = "Cieszysz się życiem jako singiel w3";  
+        baza_wolny[0] = "Cieszysz się życiem jako singiel i to jest to";
+	      baza_wolny[1] = "W Twoim życiu pojawi się ktoś ważny";
+	      baza_wolny[2] = "Jeśli dobrze sie rozejrzysz wokół siebie to zobaczysz kogoś, komu na Tobie zależy";  
 
   let baza_zwiazek = [];
-        baza_zwiazek[0] = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia w1";
-	      baza_zwiazek[1] = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia w2";
-	      baza_zwiazek[2] = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia w3";
+        baza_zwiazek[0] = "Jesteś w wolnym związku, papierek nie ma dla ciebie znaczenia";
+	      baza_zwiazek[1] = "Zdaje się, że tym roku podejmiesz nowe zobowiązania";
+	      baza_zwiazek[2] = "Hmm... chyba czeka Cię coś nowego";
 
   let baza_student = [];
-        baza_student[0] = "Studenckie życie to jest to w1";
-	      baza_student[1] = "Studenckie życie to jest to w2";
-	      baza_student[2] = "Studenckie życie to jest to w3";
+        baza_student[0] = "Studenckie życie jest właśnie dla Ciebie";
+	      baza_student[1] = "Uważaj nowy rok może przyniesć Ci wiele kłopotów, ale wszystko zależy od Ciebie";
+	      baza_student[2] = "Nauka pójdzie zdecydowanie łatwiej";
         
   let baza_pracuje = [];
-        baza_pracuje[0] = "Pracujesz - prowadzisz stabilne życie w1";
-	      baza_pracuje[1] = "Pracujesz - prowadzisz stabilne życie w2";
-	      baza_pracuje[2] = "Pracujesz - prowadzisz stabilne życie w3";
+        baza_pracuje[0] = "Pracujesz - prowadzisz stabilne życie, tak też będzie w najbliższym czasie";
+	      baza_pracuje[1] = "Zastanów się czy nie potrzebujesz nowej pracy";
+	      baza_pracuje[2] = "Nowa praca, nowi znajomi, nowe wyzwania, chyba na to czas";
 
   let baza_szuka_pracy = [];
-        baza_szuka_pracy[0] = "Spełniasz marzenia szukając nowych szans w1";
-	      baza_szuka_pracy[1] = "Spełniasz marzenia szukając nowych szans w2";
-	      baza_szuka_pracy[2] = "Spełniasz marzenia szukając nowych szans w3";    
+        baza_szuka_pracy[0] = "Spełnisz marzenia szukając nowych możliwości";
+	      baza_szuka_pracy[1] = "Znajdziesz wymarzoną pracę, ale musisz się postarać";
+	      baza_szuka_pracy[2] = "Rozejrzyj się może coś niedawno przegapiłeś";    
